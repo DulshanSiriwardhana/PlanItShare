9d9b58ee046077ca012f31cda9232e69973dcb36
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2024-04-01 09:09:03 +0530

    I Created post method and get method by Id for both plan and user

--- backend/routes/plan.js
+++ backend/routes/plan.js
@@ -6,12 +6,33 @@ const Plan = require("../models/plan");
 router.get('/',async(req,res)=>{
     try {
         const plans = await Plan.find();
-        res.json(users);
+        res.status(200).finaljson(planss);
     }
     catch (error) {
         res.status(500).json({message: error.message});
     }
 });
+router.post('/add',(req,res)=>{
+    const name = req.body.name;
 
+    const newPlan = new Plan({
+        name
+    })
+    newPlan.save().then(()=>{
+        res.status(200).json(newPlan);
+    }).catch((err)=>{
+        res.status(500).json(err);
+    })
+})
+router.get('/:planId',async(req,res)=>{
+    try {
+        const planId = req.params.planId;
+        const plan = await Plan.findById(planId);
+        res.status(200).finaljson(plan);
+    }
+    catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,6 +12,32 @@ router.get('/',async(req,res)=>{
         res.status(500).json({message: error.message});
     }
 });
+router.post('/add',(req,res)=>{
+    const name = req.body.name;
+    const password = req.body.password;
+    const regnum = req.body.regnum;
+
+    const newUser = new User({
+        name,
+        password,
+        regnum
+    })
+    newPlan.save().then(()=>{
+        res.status(200).json(newUser);
+    }).catch((err)=>{
+        res.status(500).json(err);
+    })
+})
+router.get('/:userId',async(req,res)=>{
+    try {
+        const userId = req.params.userId;
+        const user = await Plan.findById(userId);
+        res.status(200).finaljson(user);
+    }
+    catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 
 module.exports = router;
