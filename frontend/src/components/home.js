5a03ed5dcb89898ce842f283bf96d1d9fe54f3f6
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2024-04-03 19:44:19 +0530

    change in home page

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCatch } from '../catching-mechanism/catch';
 
-function Home() {
+const Home = () => {
+  const [loggedUser, setLoggedUser] = useState(null);
+
+  useEffect(()=>{
+    setLoggedUser(getCatch('loggedUser'));
+  },[]);
+
   return (
-    <div className="container-fluid">home</div>
+    <form>
+      <h3>Available Plans</h3>
+
+      <input
+        type="text"
+        placeholder="Search Plan ID..."
+        className="form-control mb-2"
+      />
+
+      <div className="list-group">
+        <button type="button" className="list-group-item list-group-item-action">Plan ID - Plan Name</button>
+        <button type="button" className="list-group-item list-group-item-action">Plan ID - Plan Name</button>
+        <button type="button" className="list-group-item list-group-item-action">Plan ID - Plan Name</button>
+        <button type="button" className="list-group-item list-group-item-action">Plan ID - Plan Name</button>
+        <button type="button" className="list-group-item list-group-item-action">Plan ID - Plan Name</button>
+      </div>
+    </form>
   )
 }
 
